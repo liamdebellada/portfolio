143fc15f59698eec42d7fde92ee81fd6a533572f
liamdebellada <liam.debell@ada.ac.uk>
2021-08-03 16:05:37 +0100

    Added animations to node tree

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -5,7 +5,38 @@ import './projects.css';
 import Heading from '../../components/heading/heading';
 
 //lib
-import {motion} from 'framer-motion';
+import {motion, useAnimation} from 'framer-motion';
+
+const PathItem = ({startNode}: {startNode?: boolean}) => {
+
+	const animation = useAnimation();
+
+	const handleComplete = () => {
+		animation.start({
+			scale: 1,
+			transition: { duration: 0.2 }
+		})
+	}
+
+	return (
+		<>
+		{startNode && (
+			<motion.div className="circle"/>
+		)}
+			<svg width="4" height="300" xmlns="http://www.w3.org/2000/svg">
+				<motion.path 
+				d="M 2,2 v 300" 
+				stroke="#000"
+				initial={{pathLength: 0}}
+				animate={{pathLength: 1}}
+				transition={{duration: 1}}
+				onAnimationComplete={handleComplete}
+				/>
+			</svg>
+			<motion.div initial={{scale: 0}} animate={animation} className="circle"/>
+		</>
+	)
+}
 
 export default function huh() {
 	return (
@@ -15,6 +46,16 @@ export default function huh() {
 				<p className="descriptionText">
 				All of my personal projects are open-source and available to fork, download and contribute to on GitHub. 
 				</p>
+			</div>
+			<div className="contentArea">
+				<div className="progressArea">
+					{[1,2,3,4].map((_, i) => (
+						<PathItem startNode={i == 0 ? true : false } />
+					))}
+				</div>
+				<div className="textArea">
+					content
+				</div>
 			</div>
 		</div>
 	)
