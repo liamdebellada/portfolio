dc66feb421ed89959334a95f1c29ca298bab22b3
liamdebellada <liam.debell@ada.ac.uk>
2021-08-03 17:27:30 +0100

    Added image layout and base image

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,22 +7,18 @@ import Heading from '../../components/heading/heading';
 //lib
 import {motion, useAnimation} from 'framer-motion';
 
-const PathItem = ({startNode}: {startNode?: boolean}) => {
-
+const PathItem = ({index} : {index: number}) => {
 	const animation = useAnimation();
-
 	const handleComplete = () => {
 		animation.start({
 			scale: 1,
-			transition: { duration: 0.1 }
+			transition: { duration: 0.3, type: "spring", damping: 9}
 		})
 	}
 
 	return (
 		<>
-		{startNode && (
-			<motion.div initial={{scale: 0}} animate={animation} className="circle"/>
-		)}
+			<motion.div initial={{scale: index == 0 ? 1 : 0}} animate={animation} className="circle"/>
 			<svg width="4" height="300" xmlns="http://www.w3.org/2000/svg">
 				<motion.path 
 				d="M 2,2 v 300" 
@@ -47,14 +43,28 @@ export default function huh() {
 				</p>
 			</div>
 
-			{[1,2,3,4].map((item, i) => (
+			{[1,2].map((item, i) => (
 				<div className="contentArea">
 					<div className="progressArea">
-						<PathItem startNode={true} />
+						<PathItem index={i}/>
 					</div>
-					<div className="textArea">
+					<motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.2}} className="textArea">
 						<Heading options={{text: "RSN Archive", size: "2rem"}}/>
-					</div>
+						<div className="contentInfo">
+							<div className="contentInfoText">
+								<p className="descriptionText">RSN Archive provides a blazing fast image browser for the Royal School of Needlework. The application is written in Rust and Svelte to provide the fastest possible performance available.</p>
+								<p className="descriptionText">Working with staff at the Royal School of Needlework, I was able to accurately meet client requirements communicating remotely through emails and video calls.</p>
+							</div>
+							<div className="contentInfoImages">
+								<div className="images">
+									<img src="/screens/needles.png"/>
+								</div>
+								<div className="buttons">
+
+								</div>
+							</div>
+						</div>
+					</motion.div>
 				</div>
 			))}
 		</div>
