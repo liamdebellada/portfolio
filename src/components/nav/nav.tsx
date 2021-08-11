161b5e06213e253d01082c7275fb958171c81182
liamdebellada <liam.debell@ada.ac.uk>
2021-08-11 18:41:21 +0100

    Converted bar list to component for re-usability

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -10,6 +10,23 @@ interface Options {
 	route: string
 }
 
+const ItemList = ({options, path} : {options: Options[], path: string}) => {
+	return (
+		<>
+			{options.map((option, i) => (
+				<div key={i} className="navItem noselect">
+					<span 
+						onClick={() => navigate(option.route)} 
+						className={option.route == path ? "navItemActive material-icons" : "material-icons"}
+						>
+							{option.icon}
+					</span>
+				</div>
+			))}
+		</>
+	)
+}
+
 const Bar = ({options, path}: {options: Options[], path: string}) => {
 
 	const [size, setSize] = React.useState<number[] | null[]>([null, null]);
@@ -30,17 +47,12 @@ const Bar = ({options, path}: {options: Options[], path: string}) => {
 	}, [expanded]);
 
 	return (
-		<motion.div transition={{duration: 1}} className={size[0] != null && size[0] > 600 ? "barParent fullSize" : "barParent smallSize" + (expanded ? " expandedNav" : "")} onClick={() => size[0] != null && size[0] > 600 ? undefined : setExpanded(expanded ? false : true) }>
-			{size[0] != null && size [0] > 600 ? options.map((option, i) => (
-				<div key={i} className="navItem noselect">
-					<span 
-						onClick={() => navigate(option.route)} 
-						className={option.route == path ? "navItemActive material-icons" : "material-icons"}
-						>
-							{option.icon}
-					</span>
-				</div>
-			)) : (
+		<motion.div className={size[0] != null && size[0] > 600 ? "barParent fullSize" : "barParent smallSize" + (expanded ? " expandedNav" : "")} onClick={() => size[0] != null && size[0] > 600 ? undefined : setExpanded(expanded ? false : true) }>
+			{size[0] != null && size [0] > 600 ? (
+				<ItemList options={options} path={path}/>
+			): expanded ? (
+				<ItemList options={options} path={path}/>
+			) : (
 				<span className="material-icons noselect">menu</span>
 			)}
 		</motion.div>
